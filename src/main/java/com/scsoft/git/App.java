ae2ea3334c928dc97bb8bf971c56dd75ab826421
yul-tong <279792150@qq.com>
2019-07-19 14:07:34 +0800

    第一下修改Git项目

--- src/main/java/com/scsoft/git/App.java
+++ src/main/java/com/scsoft/git/App.java
@@ -6,5 +6,7 @@ package com.scsoft.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("第一次修改");
+
     }
 }
