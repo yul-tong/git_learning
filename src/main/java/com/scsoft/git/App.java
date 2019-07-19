62ba7c9f3b0f1ea193bb9f4319c2a853a2aacc1a
yul-tong <279792150@qq.com>
2019-07-19 14:10:30 +0800

    第二次修改Git项目

--- src/main/java/com/scsoft/git/App.java
+++ src/main/java/com/scsoft/git/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
 
     }
 }
