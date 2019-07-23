6066b5a4d251dcdd612932a9513a80f9e10f3f22
yul-tong <279792150@qq.com>
2019-07-23 14:37:50 +0800

    first_branch分支的第二次修改

--- src/main/java/com/scsoft/git/App.java
+++ src/main/java/com/scsoft/git/App.java
@@ -9,6 +9,7 @@ public class App {
         System.out.println("第一次修改");
         System.out.println("第二次修改");
         System.out.println("这个是first_branch分支中的修改");
+        System.out.println("这个是first_branch分支中的第二次修改");
 
     }
 }
